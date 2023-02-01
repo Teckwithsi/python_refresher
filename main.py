a1c8e5f3022ef2cabfd5680a0c218094d67a6e96
simon <124110714+Teckwithsi@users.noreply.github.com>
2023-01-31 17:55:38 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,45 @@
+# is the object of the class int
+x = 1
+y = "hello"
+
+
+# one ojbect is a string and one is an interger thus can not be used unless change one or the other
+#print (x + y )
+# TypeError: unsupported operand type(s) for +: 'int' and 'str' is the error message for this case!
+# IF CHANGE BOTH OBE4CTS TO INTERGERS
+
+x =1
+y= 2
+
+print(x + y)
+
+
+# both are from the interger class thus are able to be used together
+string = "hello"
+print(string.upper())
+
+# the .upper is a mthod, u can put an argument into the brackets :0/
+# the method is acting on a specific object
+#in this case the method is going fro the oject string.
+
+# the methods used are based on the classses.
+
+# so for example
+
+#y = 1
+
+#print (y.upper())
+
+   #print (y.upper())
+           #^^^^^^^
+#AttributeError: 'int' object has no attribute 'upper
+
+
+# ok so basic class
+
+class Dog:
+    def bark():
+
+
+
+
